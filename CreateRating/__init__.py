318710155708d049501779b951581e182d857b66
Gaetan Rebufet <gaetan.rebufet@amadeus.com>
2022-03-29 15:57:19 +0200

    finished validation and trying to save in DB(not working yet)

--- CreateRating/__init__.py
+++ CreateRating/__init__.py
@@ -3,12 +3,17 @@ import uuid
 import azure.functions as func
 import time
 import requests
+import json
 
-def validateUser(user):
-    logging.info('user ' + user)
-    usercall = requests.get('https://serverlessohapi.azurewebsites.net/api/GetUser', params = {'userId': str(user)})
-    return usercall.status_code == requests.codes.ok
+def validateUser(userId):
+    logging.info('userId ' + userId)
+    call = requests.get('https://serverlessohapi.azurewebsites.net/api/GetUser', params = {'userId': userId})
+    return call.status_code == requests.codes.ok
 
+def validateProduct(productId):
+    logging.info('productId ' + productId)
+    call = requests.get('https://serverlessohapi.azurewebsites.net/api/GetProduct', params = {'productId': productId})
+    return call.status_code == requests.codes.ok
 
 def getParam(req, text):
     param = req.params.get(text)
@@ -38,12 +43,24 @@ class Rating:
         return False
 
     def isValid(self):
-        if self.isValidRating() and validateUser(self.userId):
+        if  self.isValidRating() \
+            and validateUser(self.userId) \
+            and validateProduct(self.productId):
             return True
         return False
-    
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+    def printJson(self):
+        json_data = {}
+        json_data["userId"] = self.userId
+        json_data["productId"] = self.productId
+        json_data["locationName"] = self.locationName
+        json_data["rating"] = str(self.rating)
+        json_data["userNotes"] = self.userNotes
+        json_data["id"] = self.id
+        json_data["timestamp"] = str(self.timestamp)
+        return json.dumps(json_data)
+
+def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     userId = getParam(req, 'userId')
@@ -55,7 +72,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     rating = Rating(userId, productId, locationName, rating, userNotes)
 
     if rating.isValid():
-        return func.HttpResponse(f"Hello this seems valid: " + str(vars(rating)))
+        json_doc = rating.printJson()
+        doc.set(json_doc)
+        return func.HttpResponse(json_doc)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
