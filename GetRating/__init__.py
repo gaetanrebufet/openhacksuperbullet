bfbb41a9e855316a9946f5e897f265c477e8bd0e
Gaetan Rebufet <gaetan.rebufet@amadeus.com>
2022-03-31 15:11:06 +0200

    Added sentiment analysis

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -1,13 +1,44 @@
 import logging
 import json
 import azure.functions as func
+import requests
+
+def getSentiment(notes):
+    url = "https://languageparser.cognitiveservices.azure.com/text/analytics/v3.2-preview.1/sentiment?opinionMining=true"
+    payload = {}
+    payload["documents"] = []
+    document = {}
+    document["id"] = "1"
+    document["text"] = notes
+    payload["documents"].append(document)
+
+    params = {}
+    params["Ocp-Apim-Subscription-Key"] = "cee4813143494a26854eb29e7f56ba77"
+    req = requests.post(url, headers=params, data=json.dumps(payload))
+    output = req.json()
+    logging.info("result: "+ str(output))
+
+    sentiment = {}
+    #result = json.loads(output)
+    sentiment["scores"] = output["documents"][0]["confidenceScores"]
+    sentiment["sentimentScore"] = output["documents"][0]["sentiment"]
+    if (sentiment["scores"]["negative"] > 0.7):
+        sentiment["sentimentFlag"] = "Alert"
+    else:
+        sentiment["sentimentFlag"] = "Normal"
+    return sentiment
 
 
 def main(req: func.HttpRequest, ratings: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     if ratings:
-        return func.HttpResponse(func.Document.to_json(ratings[0]))
+        json_rating = func.Document.to_json(ratings[0])
+        rating = json.loads(json_rating)
+        scores = getSentiment(rating["userNotes"])
+        rating["sentiment"] = scores
+        logging.info("scores: " + str(scores))
+        return func.HttpResponse(func.Document.to_json(rating))
     else:
         return func.HttpResponse(
              "Not found",
--- GetRatings/__init__.py
+++ GetRatings/__init__.py
@@ -1,7 +1,32 @@
 import logging
 import json
 import azure.functions as func
+import requests
 
+def getSentiment(notes):
+    url = "https://languageparser.cognitiveservices.azure.com/text/analytics/v3.2-preview.1/sentiment?opinionMining=true"
+    payload = {}
+    payload["documents"] = []
+    document = {}
+    document["id"] = "1"
+    document["text"] = notes
+    payload["documents"].append(document)
+
+    params = {}
+    params["Ocp-Apim-Subscription-Key"] = "cee4813143494a26854eb29e7f56ba77"
+    req = requests.post(url, headers=params, data=json.dumps(payload))
+    output = req.json()
+    logging.info("result: "+ str(output))
+
+    sentiment = {}
+    #result = json.loads(output)
+    sentiment["scores"] = output["documents"][0]["confidenceScores"]
+    sentiment["sentimentScore"] = output["documents"][0]["sentiment"]
+    if (sentiment["scores"]["negative"] > 0.7):
+        sentiment["sentimentFlag"] = "Alert"
+    else:
+        sentiment["sentimentFlag"] = "Normal"
+    return sentiment
 
 def main(req: func.HttpRequest, ratings: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -11,8 +36,13 @@ def main(req: func.HttpRequest, ratings: func.DocumentList) -> func.HttpResponse
         jsondocs = []
         for rating in ratings:
             jsondoc = func.Document.to_json(rating)
-            logging.info(str(jsondoc))
-            jsondocs.append(jsondoc)
+
+            data = json.loads(jsondoc)
+            scores = getSentiment(rating["userNotes"])
+            data["sentiment"] = scores
+
+            #logging.info(str(jsondoc))
+            jsondocs.append(json.dumps(data))
         finaljson = json.dumps(jsondocs)
         return func.HttpResponse(finaljson)
     else:
